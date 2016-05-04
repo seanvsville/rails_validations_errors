df9729c5bfa4b7eb9f18eca1365da2d561518ac8
seanvsville <seanvanho@gmail.com>
2016-05-04 11:35:49 -0700

    finished first, last, email validations

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,5 +1,7 @@
 class Owner < ActiveRecord::Base
-  # TODO: add validations
+  validates :first_name, presence: true, length: { maximum: 255 }
+  validates :last_name, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, uniqueness: true, case_sensitive: false, format: /@/, length: { maximum: 255 }
 
   before_save :normalize_phone_number
 
